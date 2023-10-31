e44167694b7f5041a36a45482f27d82f8c1f694d
pabcrudel <pabcrudel@gmail.com>
2023-10-31 11:57:48 +0100

    refactor: The messages are hardcoded

--- scripts/get-response/main.js
+++ scripts/get-response/main.js
@@ -1,27 +1,24 @@
 "use strict";
 
-const messages = [
-  "The machine is off. Please switch it on",
-  "This is not possible, an error has occurred.",
-  "That was a terrible score — total fail!",
-  "You know some things, but it\'s a pretty bad score. Needs improvement.",
-  "You did a passable job, not bad!",
-  "That\'s a great score, you really know your stuff.",
-  "What an amazing score! Did you cheat? Are you for real?"
-];
-
 function getResponse(score, machineActive) {
   let response;
 
   if (machineActive) {
-    if (score < 0 || score > 100) response = messages[1];
-    else if (score <= 19) response = messages[2];
-    else if (score <= 39) response = messages[3];
-    else if (score <= 69) response = messages[4];
-    else if (score <= 89) response = messages[5];
-    else response = messages[6];
+    if (score < 0 || score > 100) 
+      response = "This is not possible, an error has occurred.";
+    else if (score <= 19) 
+      response = "That was a terrible score — total fail!";
+    else if (score <= 39) 
+      response = "You know some things, but it\'s a pretty bad score. " +
+        "Needs improvement.";
+    else if (score <= 69) 
+      response = "You did a passable job, not bad!";
+    else if (score <= 89) 
+      response = "That\'s a great score, you really know your stuff.";
+    else 
+      response = "What an amazing score! Did you cheat? Are you for real?";
   }
-  else response = messages[0];
+  else response = "The machine is off. Please switch it on";
 
   return response;
 };
