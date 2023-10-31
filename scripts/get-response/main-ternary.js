ada74d6e8e001fcf87b0453c7eb5068f6df4aa3e
pabcrudel <pabcrudel@gmail.com>
2023-10-31 12:43:13 +0100

    refactor: Use a ternary operator to check the score

---
+++ scripts/get-response/main-ternary.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function getResponse(score, machineActive) {
+  let response;
+
+  if (machineActive)
+    response =
+      typeof score !== 'number' ||
+      isNaN(score) ||
+      score < 0 || score > 100
+      ?
+      "This is not possible, an error has occurred." :
+      score <= 19 ? "That was a terrible score — total fail!" :
+      score <= 39 ? 
+      "You know some things, but it\'s a pretty bad score. " +
+      "Needs improvement." :
+      score <= 69 ? "You did a passable job, not bad!" :
+      score <= 89 ? "That\'s a great score, you really know your stuff." :
+      "What an amazing score! Did you cheat? Are you for real?";
+  else response = "The machine is off. Please switch it on";
+
+  return response;
+};
